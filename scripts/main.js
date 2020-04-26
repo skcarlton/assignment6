7ca123998fbb3fcbd2810ca9ff146cd7655a133f
Sawyer Carlton <skcarlton@ufl.edu>
2020-04-26 14:53:16 -0400

    relinking script to index

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+//Array
+var members = [
+  "Dave Grohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett",
+];
+
+//object
+var tour = {
+  chicago: 'July 29th & 30th, 2018',
+  seattle: 'September 1st, 2018',
+  edmonton: 'September 4th, 2018',
+}
+
+//while loop
+var  x = 0;
+
+while (x < 5) {
+  console.log('Times running through the loop ' + x);
+
+  x++;
+}
